7d659aa8889eff80ba0e0fdd104240ed7af92ce5
Zach Moses <zmoses93@gmail.com>
2024-11-04 22:04:12 -0800

    SwgohComlinkApi module added

---
+++ lib/swgoh_comlink_api.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# API for retrieving data from the SW:GOH game
+module SwgohComlinkApi
+  def comlink_api
+    SwgohComlink.new(
+      ENV['COMLINK_URL'],
+      secret_key: ENV['COMLINK_SECRET_KEY'],
+      access_key: ENV['COMLINK_ACCESS_KEY']
+    )
+  end
+end
